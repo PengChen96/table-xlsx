c14d83d8333673d2ed2d3466a8a22bd37779c7a2
pengchen96 <2601349541@qq.com>
2021-10-23 15:06:38 +0800

    feat: Export supports multiple table headers

--- src/exportFile.ts
+++ src/exportFile.ts
@@ -59,30 +59,39 @@ const formatToSheet = ({columns, dataSource, hideHeader, raw}: {
   const headerKeys: (number|string)[] = [];
   const $cols: { wpx: any }[] = [];
   const $merges: {s: { c:number, r:number }, e: { c:number, r:number }}[] = [];
-  let headerRowsPlaceholder = 1; // 表头行占的行数
-  columns.forEach((col: any, colIndex: number) => {
+  //
+  const {columns: flatColumns, level: headerLevel} = flattenColumns(columns);
+  // 表头信息
+  const headerData = getHeaderData({columns, headerLevel});
+  Object.assign(sheet, headerData.sheet);
+  $merges.push(...headerData.merges);
+  //
+  // flatColumns = columnsInfo.columns;
+  // const headerRowsPlaceholder = level; // 表头行占的行数
+  flatColumns.forEach((col: any, colIndex: number) => {
     const key = col.dataIndex || col.key;
     const title = col.title; // todo
     header[key] = title;
     headerKeys.push(key);
     $cols.push({wpx: formatToWpx(col.width)});
     const xAxis = XLSX.utils.encode_col(colIndex);
-    const headerData = hideHeader ? [] : [{
-      [key]: title,
-    }];
+    // const headerData = hideHeader ? [] : [{
+    //   [key]: title,
+    // }];
     // todo 多级表头
-    headerRowsPlaceholder = headerData.length;
-    [...headerData, ...dataSource].forEach((data: any, rowIndex: number) => {
+    // headerRowsPlaceholder = headerData.length;
+    // [...headerData, ...dataSource].forEach((data: any, rowIndex: number) => {
+    dataSource.forEach((data: any, rowIndex: number) => {
       const value = data[key];
       if (col.render) {
         const result = col.render(value, data, rowIndex);
-        const merge = getMerge({result, colIndex, rowIndex, headerRowsPlaceholder});
+        const merge = getMerge({result, colIndex, rowIndex, headerLevel});
         if (merge) {
           $merges.push(merge);
         }
       }
-      const style = rowIndex < headerRowsPlaceholder ? headerStyle : {};
-      sheet[`${xAxis}${rowIndex + 1}`] = {
+      // const style = rowIndex < headerRowsPlaceholder ? headerStyle : {};
+      sheet[`${xAxis}${headerLevel + rowIndex + 1}`] = {
         t: (raw && typeof value === 'number') ? 'n' : 's',
         v: value,
         s: {
@@ -91,18 +100,141 @@ const formatToSheet = ({columns, dataSource, hideHeader, raw}: {
             color: { rgb: '333' },
           },
           border: defaultBorder,
-          ...style
+          // ...style
         }
       };
     });
   });
-  const xe = XLSX.utils.encode_col(columns.length - 1);
-  const ye = headerRowsPlaceholder + dataSource.length;
+  const xe = XLSX.utils.encode_col(flatColumns.length - 1);
+  const ye = headerLevel + dataSource.length;
   sheet['!ref'] = `A1:${xe}${ye}`;
   sheet['!cols'] = $cols;
   sheet['!merges'] = $merges;
   return {
     sheet
+  };
+};
+
+/**
+ *
+ */
+const getHeaderData = ({
+  columns,
+  headerLevel
+} : {
+  columns:any,
+  headerLevel:number
+}) => {
+  const sheet: any = {};
+  const merges: {s: { c:number, r:number }, e: { c:number, r:number }}[] = [];
+
+  const headerArr:any = getHeader2dArray({columns, headerLevel});
+  headerArr.forEach((rowsArr:any, rowIndex:number) => {
+    rowsArr.forEach((cols:any, colIndex:number) => {
+      const xAxis = XLSX.utils.encode_col(colIndex);
+      sheet[`${xAxis}${rowIndex + 1}`] = {
+        t: 's',
+        v: cols.title,
+        s: {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          border: defaultBorder,
+          ...headerStyle
+        }
+      };
+      if (cols.merges) {
+        merges.push(cols.merges);
+      }
+    });
+  });
+  return {
+    sheet,
+    merges
+  };
+};
+/**
+ * 获取表头二维数组
+ */
+const getHeader2dArray = ({
+  columns,
+  headerLevel
+} : {
+  columns:any,
+  headerLevel:number
+}) => {
+  const arr: any[][] = [];
+  const deal = (_columns:any, startCol = 0, rowLevel = 0) => {
+    _columns.reduce((prevCol:any, currentValue:any) => {
+      if (!arr[rowLevel]) {
+        arr[rowLevel] = [];
+      }
+      arr[rowLevel][prevCol] = {
+        title: currentValue.title,
+        children: currentValue.children,
+      };
+      let nextCol = prevCol;
+      if (currentValue.children) {
+        const childrenLen = flattenColumns(currentValue.children).columns.length;
+        deal(currentValue.children, prevCol, rowLevel + 1);
+        nextCol += childrenLen;
+        arr[rowLevel][prevCol].merges = {
+          s: {c: prevCol, r: rowLevel},
+          e: {c: nextCol - 1, r: rowLevel},
+        };
+        // 补全值 跨行的值
+        for(let c = prevCol + 1; c < nextCol; c++){
+          arr[rowLevel][c] = {
+            title: currentValue.title,
+            children: currentValue.children,
+          };
+        }
+      } else {
+        nextCol += 1;
+        // 有跨列
+        if (headerLevel - 1 - rowLevel > 0) {
+          arr[rowLevel][prevCol].merges = {
+            s: {c: prevCol, r: rowLevel},
+            e: {c: prevCol, r: headerLevel - 1},
+          };
+          // 补全值 跨列的值
+          for (let r = rowLevel + 1; r < headerLevel; r++) {
+            if (!arr[r]) {
+              arr[r] = [];
+            }
+            arr[r][prevCol] = {
+              title: currentValue.title,
+              children: currentValue.children,
+            };
+          }
+        }
+      }
+      return nextCol;
+    }, startCol);
+  };
+  deal(columns);
+  return arr;
+};
+/**
+ * 扁平化列
+ */
+const flattenColumns = (columns: any) => {
+  const newColumns: any[] = [];
+  const level: boolean[] = [];
+  const flatten = (_columns:any, index = 0) => {
+    level[index] = true;
+    index += 1;
+    _columns.forEach((v: any) => {
+      if (v.children && v.children.length > 0) {
+        flatten(v.children, index);
+      } else {
+        newColumns.push(v);
+      }
+    });
+  };
+  flatten(columns);
+  return {
+    level: level.length,
+    columns: newColumns
   };
 };
 /**
@@ -122,17 +254,17 @@ const getMerge = ({
   result,
   colIndex,
   rowIndex,
-  headerRowsPlaceholder
+  headerLevel
 }:{
   result:any,
   colIndex: number,
   rowIndex: number,
-  headerRowsPlaceholder: number
+  headerLevel: number
 }) => {
   if (result.props) {
     const {colSpan, rowSpan} = result.props;
     if ((colSpan && colSpan !== 1) || (rowSpan && rowSpan !== 1)) {
-      const realRowIndex = rowIndex + headerRowsPlaceholder;
+      const realRowIndex = rowIndex + headerLevel;
       const merge:any = {
         s: { c: colIndex, r: realRowIndex},
         e: { c: undefined, r: undefined},
---
+++ website/docs/tutorial-extras/grouping-columns.md
@@ -0,0 +1,111 @@
+---
+sidebar_position: 2
+---
+
+# 分组表头表格导出
+
+```jsx live
+function MyComponent() {
+  const [data, setData] = React.useState([]);
+  const [cols, setCols] = React.useState([]);
+  React.useEffect(() => {
+    init();
+  }, [])
+  const init = () => {
+      const columns = [{
+        title: 'title1',
+        children: [
+          {
+            title: 'title1-1',
+            children: [{
+              title: 'title1-1-1',
+              dataIndex: 'c1',
+              key: 'c1',
+              width: 100,
+            }, {
+             title: 'title1-1-2',
+             dataIndex: 'c2',
+             key: 'c2',
+             width: 100,
+            }],
+          },
+          {
+            title: 'title1-2',
+            dataIndex: 'c3',
+            key: 'c3',
+            width: 100,
+          },
+        ],
+      }, {
+        key: 'c2',
+        dataIndex: 'c2',
+        title: 'title2',
+        width: 100,
+      }, {
+        key: 'c3',
+        dataIndex: 'c3',
+        title: 'title3',
+        width: 100,
+        children: [
+          {
+            key: 'c3',
+            dataIndex: 'c3',
+            title: 'title3',
+            width: 100,
+          },
+          {
+            key: 'c3',
+            dataIndex: 'c3',
+            title: 'title3',
+            width: 100,
+          },
+        ]
+      }, {
+        key: 'c4',
+        dataIndex: 'c4',
+        title: 'title4',
+        width: 100,
+      }];
+      const dataSource = [{
+        c1: 'data1',
+        c2: 'data2',
+        c3: 'data3',
+        c4: 'data4',
+      }, {
+        c1: 11,
+        c2: 22,
+        c3: 33,
+        c4: 44,
+      }, {
+        c1: 11,
+        c2: 22,
+        c3: 33,
+        c4: 44,
+      }];
+      setData(dataSource);
+      setCols(columns);
+    };
+  const onExportFileClick = () => {
+    exportFile({
+     sheetNames: ['a'],
+     columns: cols,
+     dataSource: data,
+    });
+  };
+  return (
+    <div>
+      <button onClick={() => onExportFileClick()}>export</button>
+      <div>
+        <Table
+          style={{marginTop: 20}}
+          dataSource={data}
+          columns={cols}
+          bordered
+          size={'small'}
+          pagination={false}
+        />
+      </div>
+    </div>
+  );
+}
+```
