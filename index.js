44d0f2ba7c7ff37af96fe94cab80766489055744
pengchen96 <2601349541@qq.com>
2022-04-26 19:54:05 +0800

    fix: 暂时移除development的包

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/table-xlsx.production.js');
-} else {
-  module.exports = require('./dist/table-xlsx.development.js');
-}
+// TODO development webpack4 require(@pengchen/xlsx)有问题
+// if (process.env.NODE_ENV === 'production') {
+module.exports = require('./dist/table-xlsx.production.js');
+// } else {
+//   module.exports = require('./dist/table-xlsx.development.js');
+// }
